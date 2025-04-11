341edacc40b354810fec74c7ce12f661a73d4f75
Victor Lobe <32442832+victorlobe@users.noreply.github.com>
2025-04-12 01:00:15 +0200

    Revert "Release 1.2"
    This reverts commit b782c6986c52edfa6984dfb91cb62d07a78f53f3.

--- Netatalk Manager/AppDelegate.swift
+++ Netatalk Manager/AppDelegate.swift
@@ -15,21 +15,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
          sender.state = showDockBadge ? .on : .off
         UserDefaults.standard.set(showDockBadge, forKey: "showDockBadge")
      }
-    @IBAction func openConfig(_ sender: Any) {
-        let configPath = "/opt/homebrew/etc/afp.conf"
-        let configURL = URL(fileURLWithPath: configPath)
 
-        if FileManager.default.fileExists(atPath: configPath) {
-            NSWorkspace.shared.activateFileViewerSelecting([configURL])
-        } else {
-            let alert = NSAlert()
-            alert.messageText = "Die Konfigurationsdatei wurde nicht gefunden."
-            alert.informativeText = configPath
-            alert.alertStyle = .warning
-            alert.runModal()
-        }
-    }
-    
     func applicationDidFinishLaunching(_ notification: Notification) {
         if let window = NSApplication.shared.windows.first {
             window.styleMask.remove(.resizable)
@@ -48,10 +34,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
 
     func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
-        return true
-    }
-    
-    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
         return true
     }
 
